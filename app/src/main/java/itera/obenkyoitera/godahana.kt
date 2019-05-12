2c303df79dd16ce75efdd13dfca2803fb8bd256f
muhamadrefo <muhamad.14116023@student.itera.ac.id>
2019-05-12 20:57:46 +0700

    Tambah Godahana, Godakana

---
+++ app/src/main/java/itera/obenkyoitera/godahana.kt
@@ -0,0 +1,12 @@
+package itera.obenkyoitera
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class godahana : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_godahana)
+    }
+}
